39496c8524d5c2680f18fd0c5d7ae2a31371fcc6
Jessen Mercado <jessen.mercado@metafinanciera.com>
2021-08-05 19:17:42 -0500

    Implementacion de tailwind para despues hacer el modo oscuro

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,24 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 const Header = () => {
-    const [darkMode, setDarkMode] = useState(false)
+  const [darkMode, setDarkMode] = useState(false)
 
-    const handleClick = () => {
-        setDarkMode(!darkMode)
-    }
+  const handleClick = () => {
+    setDarkMode(!darkMode)
+  }
 
-    return (
-        <div className="Header">
-            <h1>ReactHooks</h1>
-            <button type="button" onClick={handleClick}>{darkMode ? 'Dark Mode' : 'Light Mode'}</button>
-        </div>
-    )
+  return (
+    <div className='flex justify-around w-screen mt-4'>
+      <h1>ReactHooks + Rick and Morty</h1>
+      <button
+        className='bg-white text-black px-4 py-2 rounded border-black border-2 hover:shadow-2xl'
+        type='button'
+        onClick={handleClick}
+      >
+        {darkMode ? 'Dark Mode' : 'Light Mode'}
+      </button>
+    </div>
+  )
 }
 
 export default Header
